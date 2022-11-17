35e9e6ae79dcab24e3f8f1b36d3898b22d5e9cc9
SergioGomezSantos <gomez.sergio.smr@gmail.com>
2022-11-17 16:32:16 +0100

    bindValue

--- public/preparedstatement.php
+++ public/preparedstatement.php
@@ -12,8 +12,15 @@ try {
     $sentence = $db->prepare("INSERT INTO credentials (name, password) VALUES (:name, :pass)");
     $name = "Name";
     $pass = "1234";
-    $sentence->bindParam(":name", $name);
-    $sentence->bindParam(":pass", $pass);
+
+    // $sentence->bindParam(":name", $name);   El valor de la variable puede cambiar antes del execute
+    // $sentence->bindParam(":pass", $pass);
+
+    $sentence->bindValue(":name", $name);   // El valor de la variable no puede cambiar antes del execute
+    $sentence->bindValue(":pass", $pass);
+
+    $name = "Name2";
+    $pass = "12345";
 
     $result = $sentence->execute();
     echo $result;
