5aef80d140aae075345b5aa35f5bbe24a4cd1a6e
SergioGomezSantos <gomez.sergio.smr@gmail.com>
2022-11-17 16:11:01 +0100

    Prepared Statement

--- public/preparedstatement.php
+++ public/preparedstatement.php
@@ -9,6 +9,15 @@ try {
     $db = new PDO($dsn, $user, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    $sentence = $db->prepare("INSERT INTO credentials (name, password) VALUES (:name, :pass)");
+    $name = "Name";
+    $pass = "1234";
+    $sentence->bindParam(":name", $name);
+    $sentence->bindParam(":pass", $pass);
+
+    $result = $sentence->execute();
+    echo $result;
+
 } catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
