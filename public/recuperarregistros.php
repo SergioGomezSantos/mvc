109a7d30cadec7ead73e719203d1607a63bc3fa5
SergioGomezSantos <gomez.sergio.smr@gmail.com>
2022-11-21 15:23:13 +0100

    Recuperar Registros

---
+++ public/recuperarregistros.php
@@ -0,0 +1,17 @@
+<?php
+
+class Login {
+
+    protected $nombreUsu = null;
+    protected $password = null;
+
+    public function __construct()
+    {
+
+    }
+
+    public static function all()
+    {
+        
+    }
+}
